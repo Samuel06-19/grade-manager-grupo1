de72ca10b324c08b80b17382571b0f6c5d1b67d2
javierboyko471-prog <javier.boyko.471@alu.tecnica29de6.edu.ar>
2025-10-20 20:31:06 -0300

    test_grade_manager.py

--- proyecto_github_python/test_grade_manager.py
+++ proyecto_github_python/test_grade_manager.py
@@ -1,7 +1,15 @@
-from grade_manager import calcular_promedio
-
-def test_promedio_basico():
-    assert calcular_promedio([10, 8, 6]) == 8
-
-def test_lista_vacia():
-    assert calcular_promedio([]) == 0
+import os
+from grade_manager import add_student, add_grade, average, DATA_FILE
+def setup_function():
+if os.path.exists(DATA_FILE):
+os.remove(DATA_FILE)
+def teardown_function():
+if os.path.exists(DATA_FILE):
+os.remove(DATA_FILE)
+def test_add_student_and_average():
+sid = add_student(&quot;Ana Perez&quot;)
+assert isinstance(sid, str)
+assert add_grade(sid, 8.5) is True
+assert add_grade(sid, 9.0) is True
+avg = average(sid)
+assert abs(avg - 8.75) &lt; 1e-6
